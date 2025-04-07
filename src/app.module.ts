24e730ca0e273ce09c7424ae9e7f875baf3df197
Constantin Müller <info@mueller-constantin.de>
2025-04-07 18:20:05 +0200

    feat: add basic event sourcing support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { EventSourcingModule } from '@ocoda/event-sourcing';
+import {
+  MongoDBSnapshotStore,
+  type MongoDBSnapshotStoreConfig,
+  MongoDBEventStore,
+  type MongoDBEventStoreConfig,
+} from '@ocoda/event-sourcing-mongodb';
 import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -30,6 +37,24 @@ import { AppService } from './app.service';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    EventSourcingModule.forRootAsync<
+      MongoDBEventStoreConfig,
+      MongoDBSnapshotStoreConfig
+    >({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        events: [],
+        eventStore: {
+          driver: MongoDBEventStore,
+          url: configService.get<string>('MONGO_URI')!,
+        },
+        snapshotStore: {
+          driver: MongoDBSnapshotStore,
+          url: configService.get<string>('MONGO_URI')!,
+        },
       }),
       inject: [ConfigService],
     }),
