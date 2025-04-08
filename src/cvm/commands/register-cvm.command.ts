fa73efd9546b650628dfe19903a0c35ac0d63821
Constantin Müller <info@mueller-constantin.de>
2025-04-08 17:07:23 +0200

    feat: add heuristic for handling same cvms

--- src/cvm/commands/register-cvm.command.ts
+++ src/cvm/commands/register-cvm.command.ts
@@ -3,8 +3,13 @@ import {
   type ICommand,
   type ICommandHandler,
 } from '@ocoda/event-sourcing';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CvmAggregate } from '../models';
 import { CvmEventStoreRepository } from '../repositories';
+import { Cvm } from '../repositories/schemas';
+
+const NEARBY_RADIUS_IN_METERS = 10;
 
 export class RegisterCvmCommand implements ICommand {
   constructor(
@@ -17,14 +22,30 @@ export class RegisterCvmCommand implements ICommand {
 export class RegisterCvmCommandHandler implements ICommandHandler {
   constructor(
     private readonly cvmEventStoreRepository: CvmEventStoreRepository,
+    @InjectModel(Cvm.name) private readonly cvmModel: Model<Cvm>,
   ) {}
 
   async execute(command: RegisterCvmCommand): Promise<void> {
-    const aggregate = CvmAggregate.register(
-      command.longitude,
-      command.latitude,
-    );
+    const result = await this.cvmModel
+      .findOne({
+        position: {
+          $nearSphere: {
+            $geometry: {
+              type: 'Point',
+              coordinates: [command.longitude, command.latitude],
+            },
+            $maxDistance: NEARBY_RADIUS_IN_METERS,
+          },
+        },
+      })
+      .exec();
 
-    await this.cvmEventStoreRepository.save(aggregate);
+    if (!result) {
+      const aggregate = CvmAggregate.register(
+        command.longitude,
+        command.latitude,
+      );
+      await this.cvmEventStoreRepository.save(aggregate);
+    }
   }
 }
