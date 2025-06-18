796f8f2b7e1d18d2d2bfdf90614dd976c391b446
Constantin Müller <info@mueller-constantin.de>
2025-06-18 14:22:25 +0200

    fix: prevent multiple upvoting using registrations

--- src/cvm/commands/register-cvm.command.ts
+++ src/cvm/commands/register-cvm.command.ts
@@ -6,7 +6,7 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CvmAggregate, CvmId } from '../models';
-import { CvmEventStoreRepository, Cvm } from '../repositories';
+import { CvmEventStoreRepository, Cvm, Vote } from '../repositories';
 import { CredibilityService } from 'src/ident/services';
 import { constants } from 'src/lib';
 
@@ -23,6 +23,7 @@ export class RegisterCvmCommandHandler implements ICommandHandler {
   constructor(
     private readonly cvmEventStoreRepository: CvmEventStoreRepository,
     @InjectModel(Cvm.name) private readonly cvmModel: Model<Cvm>,
+    @InjectModel(Vote.name) private readonly voteModel: Model<Vote>,
     private readonly credibilityService: CredibilityService,
   ) {}
 
@@ -57,9 +58,61 @@ export class RegisterCvmCommandHandler implements ICommandHandler {
       const aggregate = (await this.cvmEventStoreRepository.load(
         CvmId.from(result.aggregate_id),
       ))!;
+
+      // Ensure voter has not already voted
+      const hasVoted = await this.hasVotedRecently(
+        command.identity,
+        aggregate.id.value,
+      );
+
+      if (hasVoted) {
+        return;
+      }
+
       aggregate.upvote(command.identity, credibility);
 
       await this.cvmEventStoreRepository.save(aggregate);
     }
+  }
+
+  async hasVotedRecently(
+    identity: string,
+    aggregateId: string,
+  ): Promise<boolean> {
+    const recentlyLimit = new Date();
+    recentlyLimit.setDate(recentlyLimit.getDate() - constants.CVM_VOTE_DELAY);
+
+    const result = await this.voteModel.aggregate([
+      {
+        $match: {
+          identity,
+          createdAt: { $gte: recentlyLimit },
+        },
+      },
+      {
+        $lookup: {
+          from: 'cvms',
+          localField: 'cvm',
+          foreignField: '_id',
+          as: 'cvmDoc',
+        },
+      },
+      {
+        $unwind: '$cvmDoc',
+      },
+      {
+        $match: {
+          'cvmDoc.aggregate_id': aggregateId,
+        },
+      },
+      {
+        $limit: 1,
+      },
+      {
+        $project: { _id: 1 },
+      },
+    ]);
+
+    return result.length > 0;
   }
 }
