646274c0d636aaa29ab80a8a99a460fd853ead01
Constantin Müller <info@mueller-constantin.de>
2025-07-08 12:47:42 +0200

    feat: support stack trace printing in case of error

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,12 @@ async function bootstrap() {
           format: winston.format.combine(
             winston.format.colorize(),
             winston.format.timestamp(),
-            winston.format.printf(({ timestamp, level, message, context }) => {
-              // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-              return `[${timestamp}] [${level}][${context}]: ${message}`;
-            }),
+            winston.format.printf(
+              ({ timestamp, level, message, context, stack }) => {
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string
+                return `[${timestamp}] [${level}][${context}]: ${message}${stack ? `\n${stack}` : ''}`;
+              },
+            ),
           ),
         }),
         new winston.transports.File({
