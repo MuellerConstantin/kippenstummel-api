a4080364fac8dd4e7be2c5e5a664d4b4e63cf96e
Constantin Müller <info@mueller-constantin.de>
2025-10-29 22:10:17 +0100

    build: integrate migrate-mongo

---
+++ migrate-mongo-config.js
@@ -0,0 +1,14 @@
+const config = {
+  mongodb: {
+    url: process.env.MONGO_URI,
+  },
+  migrationsDir: 'migrations',
+  changelogCollectionName: 'changelog',
+  lockCollectionName: 'changelog_lock',
+  lockTtl: 0,
+  migrationFileExtension: '.js',
+  useFileHash: false,
+  moduleSystem: 'commonjs',
+};
+
+module.exports = config;
