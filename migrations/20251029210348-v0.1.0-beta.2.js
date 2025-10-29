a2967d6f1f3c4356d39a254206bbbb2521d64ca6
Constantin Müller <info@mueller-constantin.de>
2025-10-29 22:38:35 +0100

    build: add db migration for current version

---
+++ migrations/20251029210348-v0.1.0-beta.2.js
@@ -0,0 +1,60 @@
+module.exports = {
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async up(db, client) {
+    const batchSize = 1000;
+    let skip = 0;
+
+    while (true) {
+      const idents = await db
+        .collection('idents')
+        .find({})
+        .skip(skip)
+        .limit(batchSize)
+        .toArray();
+
+      if (idents.length === 0) {
+        break;
+      }
+
+      const karmaDocs = idents.map((identity) => ({
+        insertOne: {
+          document: {
+            identity: identity._id.toString(),
+            amount: 100,
+            history: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        },
+      }));
+
+      const result = await db.collection('karmas').bulkWrite(karmaDocs);
+      const insertedIds = Object.values(result.insertedIds);
+
+      const updates = idents.map((identity, index) => ({
+        updateOne: {
+          filter: { _id: identity._id },
+          update: { $set: { karma: insertedIds[index] } },
+        },
+      }));
+
+      await db.collection('idents').bulkWrite(updates);
+
+      skip += batchSize;
+    }
+  },
+
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async down(db, client) {
+    await db.collection('idents').updateMany({}, { $unset: { karma: '' } });
+    await db.collection('karmas').deleteMany({});
+  },
+};
