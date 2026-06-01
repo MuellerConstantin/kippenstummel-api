bfb186605c60db1935439b47ae2e2c53b056b42b
Constantin Müller <info@mueller-constantin.de>
2026-06-01 19:26:27 +0200

    fix: ensure imported field is a boolean when used in filter

--- src/presentation/kmc/controllers/filter.ts
+++ src/presentation/kmc/controllers/filter.ts
@@ -9,6 +9,23 @@ export class RsqlToMongoKmcCvmTransformer extends RsqlToMongoCvmTransformer {
 
     return super.isSupported(field, operator);
   }
+
+  protected transformExpression(
+    selector: string,
+    operator: string,
+    value: string | string[] | number,
+  ): object {
+    if (selector === 'imported') {
+      const boolValue = value === 'true';
+      return super.transformExpression(
+        selector,
+        operator,
+        boolValue as unknown as string,
+      );
+    }
+
+    return super.transformExpression(selector, operator, value);
+  }
 }
 
 export class RsqlToMongoKmcIdentTransformer extends RsqlToMongoIdentTransformer {
